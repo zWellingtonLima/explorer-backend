d5484f016762a85c0760b3dc6dda131d306dd3f7
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-08-16 10:17:09 +0100

    test: adds basic Testcontainers configuration and sets up with postgres:18 docker container

--- src/test/java/com/github/explorer/api/ApplicationTests.java
+++ src/test/java/com/github/explorer/api/ApplicationTests.java
@@ -2,12 +2,14 @@ package com.github.explorer.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
